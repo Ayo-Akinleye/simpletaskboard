d11a80f89c014d0cbb220eabce2adf7a6eed214b
MideJoy Akinleye <joyintech50@gmail.com>
2026-01-12 16:04:00 +0100

    logout button update

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,41 @@
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 import Signup from './pages/Signup';
 import Signin from './pages/Signin';
 import TaskBoard from './pages/TaskBoard';
 import NoPage from './pages/NoPage';
 import ProtectedRoute from './Routes/ProtectedRoute';
+import PublicRoute from './Routes/PublicRoute';
+
 
 const App = () => {
+
   return (
     <div>
       <BrowserRouter>
         <Routes>
-          <Route index element={<Signup />} />
-          <Route path='/signup' element={<Signup />} />
-          <Route path='/signin' element={<Signin />} />
+
+          <Route index element={<Navigate to="/signup" replace />} />
+
+          <Route path='/signup' element={
+            <PublicRoute>
+              <Signup />
+            </PublicRoute>
+          } />
+
+          <Route path='/signin' element={
+            <PublicRoute>
+              <Signin />
+            </PublicRoute>
+          } />
+
           <Route path='/taskboard' element={
             <ProtectedRoute>
               <TaskBoard />
             </ProtectedRoute>
           } />
+
           <Route path='*' element={<NoPage />} />
+
         </Routes>
       </BrowserRouter>
     </div>
