20133470c10bad2f1ad454c89ad30b902d170c09
MideJoy Akinleye <joyintech50@gmail.com>
2026-05-14 12:17:59 +0100

    page not found update

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,9 +1,12 @@
-import React from 'react'
+import { Link } from 'react-router-dom';
 
 const NoPage = () => {
   return (
     <div>
       <h1>Error: Page Not Found.</h1>
+      <p>Return to 
+        <Link to="/signin" className='text-pink-700 underline cursor-pointer'> Sign in</Link> page
+      </p>
     </div>
   )
 }
