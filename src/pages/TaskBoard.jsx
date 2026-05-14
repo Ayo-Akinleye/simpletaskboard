db78669d155cb7e760a60ebac21b7e4b2fc07947
MideJoy Akinleye <joyintech50@gmail.com>
2026-05-14 18:27:50 +0100

    tashboard header update

--- src/pages/TaskBoard.jsx
+++ src/pages/TaskBoard.jsx
@@ -40,23 +40,28 @@ const TaskBoard = () => {
   return (
     <div className='min-h-screen w-screen flex flex-col'>
       <div className='flex flex-col items-center py-8 gap-4'>
-        <div className='grid grid-cols-3 items-center w-full px-6'>
-          <button
-            className='justify-self-start cursor-pointer hover:text-red-500'
-            aria-label='logout'
-            onClick={handleLogout}>
-            <LogOut size={20} />
-          </button>
-          <h1 className='text-2xl font-bold ml-17'>
+
+        {/* welcome and logout button */}
+        <div className='flex items-center w-full px-6 h-16 relative'>
+          <h1 className='absolute left-1/2 -translate-x-1/2 m-0 whitespace-nowrap text:lg md:text-2xl font-bold'>
             {user ? `Welcome, ${user.fullname}` : "Welcome"}
           </h1>
+          <button
+            className='ml-auto cursor-pointer hover:text-red-500'
+            aria-label='logout'
+            onClick={handleLogout}
+          >
+            <LogOut size={20} />
+          </button>
 
         </div>
 
+        {/* input bar for task */}
         <TaskboardInput taskList={taskList} setTaskList={setTaskList} />
       </div>
 
-      <div className='flex flex-col gap-4 sm:grid sm:grid-cols-3 px-4 sm:px-8 md:px-10 lg:px-12'>
+      {/* actual tasks display */}
+      <div className='flex flex-col gap-4 sm:grid sm:grid-cols-3 px-4 sm:px-8 md:px-10 lg:px-12 bg-red-500'>
         {taskList.map((task, index) =>
           <Board key={index} task={task} index={index} taskList={taskList} setTaskList={setTaskList}
           />
